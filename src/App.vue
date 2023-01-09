f989d79bc572eb64338e0f1f0e1177b48582475e
NickPoint <nikita.voevudsky@gmail.com>
2023-01-10 01:18:35 +0200

    The font is set for "JetBrains Mono" The App.vue is ready for work

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+    <Header></Header>
+    <router-view></router-view>
   </div>
 </template>
 
+<script setup lang="ts">
+import Header from "@/components/Header.vue";
+</script>
+
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap");
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "JetBrains Mono", monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #fff;
+  background-color: #19191c;
 }
 
 nav {
