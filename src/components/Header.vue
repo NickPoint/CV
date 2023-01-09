8cb5f736fb3a3ad4cd79102a8e9fd9196a70de6d
NickPoint <nikita.voevudsky@gmail.com>
2023-01-10 01:21:07 +0200

    1 example how navbar can look like navigation should be further extended

---
+++ src/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container-fluid header">
+    <div class="justify-content-between d-flex align-items-center">
+      <div class="site-logo">
+        <img class="logo" src="../assets/logo-light.png" />
+        <h2>Mykyta<br />Voievudskyi</h2>
+      </div>
+      <nav>
+        <router-link to="/main">Main</router-link>
+        |
+        <router-link to="/about">About</router-link>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+}
+
+.header {
+  position: fixed;
+  top: 50px;
+  z-index: 5;
+}
+
+.header > div {
+  width: 90%;
+  margin: auto;
+  background-color: #585e63;
+  border-radius: 50px;
+  padding: 0 50px;
+}
+
+.site-logo {
+  display: flex;
+  align-items: center;
+}
+
+.site-logo h2 {
+  font-size: 1rem;
+}
+
+.site-logo .logo {
+  width: 50px;
+  margin-right: 10px;
+}
+</style>
