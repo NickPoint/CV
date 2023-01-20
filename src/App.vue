2e16e66da380e077fc2082bfe1f4de355d4bcd2c
NickPoint <nikita.voevudsky@gmail.com>
2023-01-20 10:29:39 +0200

    added global custom variables for styling

--- src/App.vue
+++ src/App.vue
@@ -11,24 +11,17 @@ import Header from "@/components/Header.vue";
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap");
+
+@import "assets/custom-vars";
 #app {
   font-family: "JetBrains Mono", monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #fff;
-  background-color: #19191c;
+  color: $text;
+  background-color: $third;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.container > * {
+  margin-bottom: $margin-in-each-container;
 }
 </style>
