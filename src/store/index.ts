da24535206f6c7f77189ed49d555a5a7395ac04a
NickPoint <nikita.voevudsky@gmail.com>
2023-02-05 13:22:32 +0200

    Added extra info about my skills

--- src/store/index.ts
+++ src/store/index.ts
@@ -13,10 +13,40 @@ export default new Vuex.Store({
       "good teamworker",
     ],
     codingSkills: {
-      Python: 70,
-      Java: 85,
-      JavaScript: 70,
-      "HTML/CSS": 95,
+      Python: [
+        70,
+        'I participated in the project "Clustering Python programs". The main goal of this project was to find clusters of similar programs. This is meant to help to detect collaboration and discover different approaches to the task.',
+        ["https://github.com/NickPoint/Clustering-Python-Programs"],
+      ],
+      Java: [
+        85,
+        "The most worthy project that I have ever done in Java is a POS (Point of sale) application. It helped me to obtain a basic knowledge of software engineering and primary skills for working at any stage of software development projects. Also, a big plus was a strong knowledge of the OOP paradigm in Java.",
+        ["https://github.com/NickPoint/POS-system"],
+      ],
+      JavaScript: [
+        70,
+        "I mainly used it when developing sites to work with the DOM. In addition, I am familiar with the Vue.js framework, the jQuery library, and a bit of the Node.js runtime and its Express framework. For example, this site is implemented with Vue.js.",
+        [document.location],
+      ],
+      "HTML/CSS": [
+        90,
+        "It is an integral part of front-end web development😃.",
+        ["https://medcut.ee/", "https://koduparadiis.ee/", "https://jum.ee/"],
+      ],
+    },
+    languageSkills: {
+      English: [
+        60,
+        75,
+        "I have an FCE level B2 certificate. However, I lack speaking practice and am willing to improve it",
+      ],
+      Estonian: [
+        75,
+        75,
+        "Since I have been living in Estonia for 5 years now, my language level is the maximum I could achieve during this period. Even so, I increase my skills extremely fast",
+      ],
+      Russian: [100, 100, "My mother tongue language"],
+      Ukrainian: [80, 80, "My second language"],
     },
   },
   getters: {
@@ -25,6 +55,9 @@ export default new Vuex.Store({
     },
     getCodingSkills(state) {
       return state.codingSkills;
+    },
+    getLanguageSkills(state) {
+      return state.languageSkills;
     },
   },
   mutations: {},
